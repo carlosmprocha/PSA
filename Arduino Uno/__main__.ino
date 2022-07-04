7702c9172d72a8db9e4fbe9ab85677f5de5dd318
carlosmprocha <92697812+carlosmprocha@users.noreply.github.com>
2022-07-04 10:40:09 +0100

    Update __main__.ino

--- Arduino Uno/__main__.ino
+++ Arduino Uno/__main__.ino
@@ -35,9 +35,9 @@
 
 #define MAX_DISTANCE 300
 #define MIN_DISTANCE 15
-#define MIN_DISTANCE_INT 11
-#define MAX_DISTANCE_INT 25
-#define LONGE 50
+#define MIN_DISTANCE_INT 440
+#define MAX_DISTANCE_INT 470
+#define LONGE 490
 
 #define MIN_TURN 20
 #define MAX_TURN 145
